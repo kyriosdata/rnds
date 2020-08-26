e03c456c78eea792826e33355269d8a18e484929
kyriosdata <fabio@inf.ufg.br>
2020-08-26 08:57:56 -0300

    rnds-lib

---
+++ projetos/lib/src/test/java/com/github/kyriosdata/rnds/TokenTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020.
+ * Fábio Nogueira de Lucena - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package com.github.kyriosdata.rnds;
+
+public class TokenTest {
+
+    /**
+     * Obtém path completo do nome do arquivo fornecido que se encontra
+     * no diretório resources.
+     *
+     * @param arquivo Nome do arquivo contido no diretório "resources".
+     * @return O caminho completo para o arquivo cujo nome é fornecido.
+     */
+    static String fromResource(final String arquivo) {
+        Class<Token> appClass = Token.class;
+        return appClass.getClassLoader().getResource(arquivo).getPath();
+    }
+}
+
+
