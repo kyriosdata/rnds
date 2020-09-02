8ee69fc488eacdb547b668bb7d0dd85f6ec5a727
kyriosdata <fabio@inf.ufg.br>
2020-09-02 10:17:47 -0300

    documentacao

--- guia/sidebars.js
+++ guia/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Tópicos: ["contexto", "si", "doc1", "doc2", "doc3"],
+    Tópicos: ["contexto", "si", "ambientes", "doc1", "doc2", "doc3"],
     Features: ["mdx"],
   },
 };
