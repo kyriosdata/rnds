6198864dfd7160816633a465e8608e5aa379d169
kyriosdata <fabio@inf.ufg.br>
2020-09-16 10:29:18 -0300

    documentacao

--- projetos/rnds-js/token.js
+++ projetos/rnds-js/token.js
@@ -1,11 +1,16 @@
 const fs = require("fs");
-const https = require("https");
+const https = require("follow-redirects").https;
 
 const auth = process.env.RNDS_AUTH;
 const ehr = process.env.RNDS_EHR;
 const certificado = process.env.RNDS_CERTIFICADO_ENDERECO;
 const senha = process.env.RNDS_CERTIFICADO_SENHA;
 const requisitante = process.env.RNDS_REQUISITANTE_CNS;
+
+/**
+ * Cache access toke)
+ */
+let accessToken = undefined;
 
 console.log(auth);
 console.log(ehr);
@@ -21,10 +26,35 @@ console.log(requisitante);
  * para esta função quando recuperado.
  */
 function token(callback) {
+  if (accessToken !== undefined) {
+    return callback(accessToken);
+  }
+
   const req = https.request(
     {
       hostname: auth,
       path: "/api/token",
+      method: "GET",
+      pfx: fs.readFileSync(certificado),
+      passphrase: senha,
+    },
+    (res) => {
+      res.on("data", function (data) {
+        console.log("recebido");
+        accessToken = JSON.parse(data.toString()).access_token;
+        callback(accessToken);
+      });
+    }
+  );
+
+  req.end();
+}
+
+function cnes(searchCnes) {
+  const req = https.request(
+    {
+      hostname: ehr,
+      path: "Organization/" + searchCnes,
       method: "GET",
       pfx: fs.readFileSync(certificado),
       passphrase: senha,
