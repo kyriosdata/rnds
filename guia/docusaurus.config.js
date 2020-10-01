93597e0ed0a6bfec73b52d643578bc4dde6316bb
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2020-10-01 17:48:37 -0300

    documentação

--- guia/docusaurus.config.js
+++ guia/docusaurus.config.js
@@ -67,6 +67,10 @@ module.exports = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Engenharia de Software (UFG).`,
     },
+    algolia: {
+      apiKey: "25626fae796133dc1e734c6bcaaeac3c",
+      indexName: "docsearch",
+    },
   },
   presets: [
     [
