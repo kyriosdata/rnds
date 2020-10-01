2431dd845310f7b78093b1e01ef4cff302f58c1f
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2020-10-01 10:47:05 -0300

    documentação

--- guia/docusaurus.config.js
+++ guia/docusaurus.config.js
@@ -61,7 +61,7 @@ module.exports = {
             {
               label: "Notícias",
               to: "blog",
-            }
+            },
           ],
         },
       ],
@@ -84,6 +84,12 @@ module.exports = {
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
+        },
+        sitemap: {
+          cacheTime: 600 * 1000, // 600 sec - cache purge period
+          changefreq: "weekly",
+          priority: 0.5,
+          trailingSlash: false,
         },
       },
     ],
