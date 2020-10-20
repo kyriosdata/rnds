0640dea3fc21abdd338363797266d9080d5d8875
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2020-10-20 13:42:19 -0300

    documentação

--- guia/src/pages/index.js
+++ guia/src/pages/index.js
@@ -68,18 +68,32 @@ function Home() {
     >
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                "button button--outline button--secondary button--lg",
-                styles.getStarted
-              )}
-              to={useBaseUrl("docs/intro/passo-a-passo")}
-            >
-              Passo a passo
-            </Link>
+          <div class="row">
+            <div class="col col--6 text--left">
+              <h1 className="hero__title titulo">
+                Rede Nacional de Dados em Saúde ({siteConfig.title})
+              </h1>
+              <p className="hero__subtitle">{siteConfig.tagline}</p>
+              <div>
+                <Link
+                  className={clsx(
+                    "button button--outline button--secondary button--lg",
+                    styles.getStarted
+                  )}
+                  to={useBaseUrl("docs/intro/passo-a-passo")}
+                >
+                  Passo a passo
+                </Link>
+              </div>
+            </div>
+
+            <div class="col col--4 text--left introducao">
+              O Guia de Integração RNDS tem como objetivo orientar o
+              desenvolvimento de solução tecnológica para a notificação
+              obrigatória de resultados de exames (SARS-CoV-2) ao Ministério da
+              Saúde.
+            </div>
+            <div class="col col--6"></div>
           </div>
         </div>
       </header>
