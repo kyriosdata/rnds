5243049124306ccfb17d697e7235e8833acdf234
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2021-01-03 15:51:24 -0300

    fhir server

---
+++ guia/docs/rel/tools/fhirserver.md
@@ -0,0 +1,8 @@
+---
+id: fhirserver
+title: Servidor FHIR
+sidebar_label: Servidor FHIR
+---
+
+A experimentação com o FHIR pode ser feita com vários servidores de [acesso livre](../../tecnologias#implementa%C3%A7%C3%A3o-do-fhir),
+disponíveis na internet ou pode-se disponibilizar um servidor local.
--- guia/sidebars.js
+++ guia/sidebars.js
@@ -1,62 +1,65 @@
 module.exports = {
   someSidebar: [
-    "introducao", "servicos", "definicoes", "tecnologias", "ambientes",
-  {
-    "Resultado de Exame Laboratorial": [
-      "rel/objetivo-rel",
-      "rel/mi-rel",
-      "rel/mc-rel",
-      "rel/intro/passo-a-passo",
-      "rel/intro/publico-alvo",
-      "rel/intro/contexto",
-      "rel/intro/glossario",
-      {
-
-        
-        Gestor: [
-          "rel/gestor/gestor",
-          "rel/gestor/certificado",
-          "rel/gestor/gov.br",
-          "rel/gestor/portal",
-          "rel/gestor/identificador",
-        ],
-        TI: [
-          "rel/ti/ti",
-          "rel/ti/conhecer",
-          "rel/ti/si",
-          "rel/ti/homologar",
-          "rel/ti/producao",
-          "rel/ti/requisicoes",
-          "rel/ti/erros",
-          "rel/ti/autenticacao",
-        ],
-        Ferramentas: [
-          "rel/tools/api",
-          "rel/tools/bibliotecas",
-          "rel/tools/fhirpath",
-          "rel/tools/keytool",
-          "rel/tools/validacao",
-        ],
-      },
-    ],
-    "Sumário de Alta": [
-      "sa/objetivo-sa", 
-      "sa/mi-sa", 
-      "sa/mc-sa", 
-      "sa/passo-a-passo-sa", 
-      "sa/pa-sa", 
-      "sa/contexto-sa",
-      "sa/glossario-sa"
-    ],
-    "Registro de Atendimento Clínico": [
-      "rac/objetivo-rac", 
-      "rac/mi-rac", 
-      "rac/mc-rac", 
-      "rac/passo-a-passo-rac", 
-      "rac/pa-rac", 
-      "rac/contexto-rac",
-      "rac/glossario-rac"
-    ],
-  }],
+    "introducao",
+    "servicos",
+    "definicoes",
+    "tecnologias",
+    "ambientes",
+    {
+      "Resultado de Exame Laboratorial": [
+        "rel/objetivo-rel",
+        "rel/mi-rel",
+        "rel/mc-rel",
+        "rel/intro/passo-a-passo",
+        "rel/intro/publico-alvo",
+        "rel/intro/contexto",
+        "rel/intro/glossario",
+        {
+          Gestor: [
+            "rel/gestor/gestor",
+            "rel/gestor/certificado",
+            "rel/gestor/gov.br",
+            "rel/gestor/portal",
+            "rel/gestor/identificador",
+          ],
+          TI: [
+            "rel/ti/ti",
+            "rel/ti/conhecer",
+            "rel/ti/si",
+            "rel/ti/homologar",
+            "rel/ti/producao",
+            "rel/ti/requisicoes",
+            "rel/ti/erros",
+            "rel/ti/autenticacao",
+          ],
+          Ferramentas: [
+            "rel/tools/api",
+            "rel/tools/bibliotecas",
+            "rel/tools/fhirpath",
+            "rel/tools/keytool",
+            "rel/tools/validacao",
+            "rel/tools/fhirserver",
+          ],
+        },
+      ],
+      "Sumário de Alta": [
+        "sa/objetivo-sa",
+        "sa/mi-sa",
+        "sa/mc-sa",
+        "sa/passo-a-passo-sa",
+        "sa/pa-sa",
+        "sa/contexto-sa",
+        "sa/glossario-sa",
+      ],
+      "Registro de Atendimento Clínico": [
+        "rac/objetivo-rac",
+        "rac/mi-rac",
+        "rac/mc-rac",
+        "rac/passo-a-passo-rac",
+        "rac/pa-rac",
+        "rac/contexto-rac",
+        "rac/glossario-rac",
+      ],
+    },
+  ],
 };
-
