e238f6a9199b507e5edfa969592d415d13c304ec
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2021-01-21 07:31:58 -0300

    documentação

---
+++ guia/src/pages/_emdesenvolvimento.js
@@ -0,0 +1,12 @@
+export const EmDesenvolvimento = ({ children, color }) => (
+  <span
+    style={{
+      backgroundColor: color,
+      borderRadius: "2px",
+      color: "#fff",
+      padding: "0.2rem",
+    }}
+  >
+    {children}
+  </span>
+);
