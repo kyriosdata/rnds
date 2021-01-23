a448666baebddf407f73e98dec04d77495e99925
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2021-01-23 10:57:46 -0300

    ilustra uso de componente em mdx

--- guia/src/pages/_emdesenvolvimento.js
+++ guia/src/pages/_emdesenvolvimento.js
@@ -1,12 +1,5 @@
-export const EmDesenvolvimento = ({ children, color }) => (
-  <span
-    style={{
-      backgroundColor: color,
-      borderRadius: "2px",
-      color: "#fff",
-      padding: "0.2rem",
-    }}
-  >
-    {children}
-  </span>
-);
+import React from "react";
+
+export default function EmDesenvolvimento() {
+  return <span className="emDesenvolvimento">Estamos trabalhando...</span>;
+}
