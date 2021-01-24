8ac7f9117edfdc66141d376de923875eefd5c5cc
kyriosdata <fabio@inf.ufg.br>
2021-01-24 17:43:22 -0200

    consulta pacientes

--- projetos/cliente-java/src/main/java/com/github/kyriosdata/rnds/BuscaPacientes.java
+++ projetos/cliente-java/src/main/java/com/github/kyriosdata/rnds/BuscaPacientes.java
@@ -1,5 +1,8 @@
 package com.github.kyriosdata.rnds;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 
@@ -8,11 +11,15 @@ import ca.uhn.fhir.rest.client.api.IGenericClient;
 
 public class BuscaPacientes {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(BuscaPacientes.class);
+
         FhirContext ctx = FhirContext.forR4();
         final String SERVER = "http://hapi.fhir.org/baseR4";
         IGenericClient client = ctx.newRestfulGenericClient(SERVER);
         Bundle resposta = client.search().forResource(Patient.class).returnBundle(Bundle.class).execute();
-        boolean peloMenosUm = !resposta.getEntry().isEmpty();
-        System.out.println("Pelo menos um? " + peloMenosUm);
+        final String peloMenosUm = resposta.getEntry().isEmpty() ? "NÃO" : "SIM";
+
+        logger.info("Acessando servidor {}", SERVER);
+        logger.info("Pelo menos um paciente? {}", peloMenosUm);
     }
 }
