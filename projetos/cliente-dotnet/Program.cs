457ac0113667e04a4fd94b06ce642686e0ffed9a
kyriosdata <fabio@inf.ufg.br>
2021-02-13 10:29:18 -0300

    dotnet

---
+++ projetos/cliente-dotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cliente_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
