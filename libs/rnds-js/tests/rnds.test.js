fbc9a033fc5b022e43c06c69c042e0b9b4bd0379
kyriosdata <fabio@inf.ufg.br>
2021-09-01 15:42:30 -0300

    pequenos ajustes

--- libs/rnds-js/tests/rnds.test.js
+++ libs/rnds-js/tests/rnds.test.js
@@ -1,14 +1,16 @@
 const { RNDS } = require("../rnds");
 
-test("busca por cns", async () => {
-  const rnds = new RNDS(false, true);
+test("busca por cnpj", async (done) => {
+  const rnds = await RNDS.cliente(false, true, false);
+  console.log(rnds);
   const pessoaJuridica = await rnds.cnpj("01567601000143");
   const dados = JSON.parse(pessoaJuridica.retorno);
   expect(dados.name).toBe("UNIVERSIDADE FEDERAL DE GOIAS");
+  done();
 });
 
 test("capability statement and token check", async (done) => {
-  const cliente = await RNDS.cliente(false, true, true);
+  const cliente = await RNDS.cliente(false, true, false);
   expect(cliente).toBeDefined();
   done();
 });
