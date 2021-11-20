b3430fdfd18c6cf98a1031c12beb9cd127b41d71
kyriosdata <fabio@inf.ufg.br>
2021-11-20 15:25:09 -0300

    Experimentando modelo de solução.

--- tools/fhirpathcli/src/main/java/com/github/kyriosdata/rnds/FhirPathApp.java
+++ tools/fhirpathcli/src/main/java/com/github/kyriosdata/rnds/FhirPathApp.java
@@ -6,7 +6,9 @@ import org.hl7.fhir.r4.hapi.fluentpath.FhirPathR4;
 import org.hl7.fhir.r4.model.Base;
 import org.hl7.fhir.r4.model.Property;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FhirPathApp {
     public static void main(String[] args) {
@@ -14,19 +16,48 @@ public class FhirPathApp {
         FhirPathR4 fp = new FhirPathR4(ctx);
 
         IBase patient = Serializacao.fromJson("patient.json");
-        List<Base> evaluate = fp.evaluate(patient, "Patient.telecom.where(rank.hasValue())", Base.class);
-        System.out.println(evaluate.get(0).getClass());
-        System.out.println(String.format("Resultados: %d", evaluate.size()));
-        System.out.println(String.format("Recurso: %b", evaluate.get(0).isResource()));
-        System.out.println(String.format("Primitivo: %b", evaluate.get(0).isPrimitive()));
-        Base base = evaluate.get(0);
+        String c1 = "Patient.telecom.where(rank.hasValue())";
+        String c2 = "Patient.name.exists()";
+        String c3 = "Patient.name";
+        List<Base> resposta = fp.evaluate(patient, c3, Base.class);
+
+        showTotal(resposta);
+        showTipos(resposta);
+        System.out.println(resposta.get(0).fhirType());
+        System.out.println(String.format("isResource: %b", resposta.get(0).isResource()));
+        System.out.println(String.format("isPrimitive: %b", resposta.get(0).isPrimitive()));
+        Base base = resposta.get(0);
         for (Property property : base.children()) {
             System.out.println(property + " " + property.getValues());
         }
 
         // IParser parser = ctx.newJsonParser().setPrettyPrint(true);
         // String json =
-        // parser.encodeResourceToString(evaluate.get(0).castToResource(evaluate.get(0)));
+        // parser.encodeResourceToString(resposta.get(0).castToResource(resposta.get(0)));
         // System.out.println(json);
+    }
+
+    public static void showTotal(List<Base> resposta) {
+        System.out.println(String.format("Total de elementos: %d", resposta.size()));
+    }
+
+    public static void showTipos(List<Base> resposta) {
+        List<String> tipos = resposta.stream()
+                .map(i -> i.fhirType()).collect(Collectors.toList());
+        System.out.println(String.format("Tipos: %s", linhaComIndice(tipos)));
+    }
+
+    public static String linha(List<String> resposta) {
+        return String.join(" ", resposta);
+    }
+
+    public static String linhaComIndice(List<String> resposta) {
+        int indice = 0;
+        final List<String> itens = new ArrayList<>(resposta.size());
+        for (String componente: resposta) {
+            itens.add(String.format("[%d]%s", indice++, componente));
+        }
+
+        return linha(itens);
     }
 }
