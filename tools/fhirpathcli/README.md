43abc2948e36314f6db2a4394594a26304e7d509
kyriosdata <fabio@inf.ufg.br>
2021-12-21 18:33:25 -0300

    experimentando avaliação de FHIRPath

--- tools/fhirpathcli/README.md
+++ tools/fhirpathcli/README.md
@@ -1,17 +1,25 @@
-﻿## Objetivo
+﻿# FhirPathCli
 
-Oferecer recurso para execução de sentenças em 
-[FHIRPath](http://hl7.org/fhirpath/) via linha de comandos.
-Ou seja, dado um recurso e uma sentença em FHIRPath, a 
-sentença é executada, possivelmente visando a localização de 
-informação ou avaliação da condição definida.
+> Oferecer recurso para execução de sentenças em 
+> [FHIRPath](http://hl7.org/fhirpath/) via linha de comandos.
 
+Esta linguagem foi desenvolvida com foco na definição de expressões 
+lógicas e consultas sobre recursos [FHIR](https://www.hl7.org/fhir/). 
+
+## Análise
+
+A entrada para o aplicativo é formada por recurso(s) FHIR e a sentença
+em FHIRPAth. A saída é o resultado da execução da sentença sobre o recurso ou
+recursos fornecidos. 
+
+## Outros
 A implementação de referência encontra-se disponível no
 [GitHub](https://github.com/hapifhir/hapi-fhir).
 
 A [documentação](https://hapifhir.io/hapi-fhir/docs/appendix/javadocs.html)
 da implementação de referência encontra-se documentada e disponível. Em particular:
 
+- Experimentação via navegador usando [FHIRPath](https://hl7.github.io/fhirpath.js/)
 - [Core base](https://hapifhir.io/hapi-fhir/apidocs/hapi-fhir-base/)
 - [Model R4](https://hapifhir.io/hapi-fhir/apidocs/hapi-fhir-structures-r4/)
 
--- tools/fhirpathcli/src/main/java/com/github/kyriosdata/rnds/FhirPathApp.java
+++ tools/fhirpathcli/src/main/java/com/github/kyriosdata/rnds/FhirPathApp.java
@@ -4,7 +4,10 @@ import ca.uhn.fhir.context.FhirContext;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.r4.hapi.fluentpath.FhirPathR4;
 import org.hl7.fhir.r4.model.Base;
+import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Property;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.codesystems.BundleType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +19,23 @@ public class FhirPathApp {
         FhirPathR4 fp = new FhirPathR4(ctx);
 
         IBase patient = Serializacao.fromJson("patient.json");
+        IBase outro = Serializacao.fromJson("patient-outro.json");
+
+        // Montar um bundle
+        Bundle bundle = new Bundle();
+        Bundle.BundleEntryComponent entry1 = new Bundle.BundleEntryComponent();
+        entry1.setResource((Resource)patient);
+        Bundle.BundleEntryComponent entry2 = new Bundle.BundleEntryComponent();
+        entry2.setResource((Resource)outro);
+
+        bundle.addEntry(entry1);
+        bundle.addEntry(entry2);
+
         String c1 = "Patient.telecom.where(rank.hasValue())";
         String c2 = "Patient.name.exists()";
         String c3 = "Patient.name";
+
+        // FIXME quando bundle substitui patient um erro ocorre
         List<Base> resposta = fp.evaluate(patient, c3, Base.class);
 
         showTotal(resposta);
