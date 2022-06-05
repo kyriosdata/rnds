fc10ef99983a69e87ef8dc85b1c8781bbdc12dcc
kyriosdata <fabio@inf.ufg.br>
2022-06-05 19:03:30 -0300

    change date format

--- tools/fhirpathcli/src/main/java/com/github/kyriosdata/rnds/FhirPathApp.java
+++ tools/fhirpathcli/src/main/java/com/github/kyriosdata/rnds/FhirPathApp.java
@@ -1,85 +1,32 @@
 package com.github.kyriosdata.rnds;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.api.IElement;
+import ca.uhn.fhir.parser.IParser;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.r4.hapi.fluentpath.FhirPathR4;
 import org.hl7.fhir.r4.model.Base;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Property;
-import org.hl7.fhir.r4.model.Resource;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FhirPathApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FhirContext ctx = FhirContext.forR4();
         FhirPathR4 fp = new FhirPathR4(ctx);
 
-        FhirPathLib fpl = new FhirPathLib();
+        IParser json = ctx.newJsonParser();
 
-        IBase patient = fpl.fromJson(FhirPathLib.class.getClassLoader().getResourceAsStream("patient.json"));
-        IBase outro = fpl.fromJson(FhirPathLib.class.getClassLoader().getResourceAsStream("patient-outro.json"));
+        InputStream fis = Files.newInputStream(Paths.get(args[0]));
 
-        // Montar um bundle
-        Bundle bundle = new Bundle();
-        Bundle.BundleEntryComponent entry1 = new Bundle.BundleEntryComponent();
-        entry1.setResource((Resource)patient);
-        Bundle.BundleEntryComponent entry2 = new Bundle.BundleEntryComponent();
-        entry2.setResource((Resource)outro);
+        IBase medicationRequest = json.parseResource(fis);
 
-        bundle.addEntry(entry1);
-        bundle.addEntry(entry2);
+        String c3 = "authoredOn.replaceMatches('\\\\b(?<year>\\\\d{4})-(?<month>\\\\d{2})-(?<day>\\\\d{2})\\\\b', '${day}/${month}/${year}')";
 
-        String c1 = "Patient.telecom.where(rank.hasValue())";
-        String c2 = "Patient.name.exists()";
-        String c3 = "Patient.name";
+        List<Base> resposta = fp.evaluate(medicationRequest, c3, Base.class);
 
-        // FIXME quando bundle substitui patient um erro ocorre
-        List<Base> resposta = fp.evaluate(patient, c3, Base.class);
-
-        showTotal(resposta);
-        showTipos(resposta);
-        Base item = resposta.get(0);
-        System.out.println(item.fhirType());
-        System.out.println(String.format("isResource: %b", item.isResource()));
-        System.out.println(String.format("isPrimitive: %b", item.isPrimitive()));
-        System.out.println(String.format("isElement: %b", item instanceof IElement));
-
-        Base base = item;
-        for (Property property : base.children()) {
-            System.out.println(property + " " + property.getValues());
-        }
-
-        // IParser parser = ctx.newJsonParser().setPrettyPrint(true);
-        // String json =
-        // parser.encodeResourceToString(resposta.get(0).castToResource(resposta.get(0)));
-        // System.out.println(json);
-    }
-
-    public static void showTotal(List<Base> resposta) {
-        System.out.println(String.format("Total de elementos: %d", resposta.size()));
-    }
-
-    public static void showTipos(List<Base> resposta) {
-        List<String> tipos = resposta.stream()
-                .map(i -> i.fhirType()).collect(Collectors.toList());
-        System.out.println(String.format("Tipos: %s", linhaComIndice(tipos)));
-    }
-
-    public static String linha(List<String> resposta) {
-        return String.join(" ", resposta);
-    }
-
-    public static String linhaComIndice(List<String> resposta) {
-        int indice = 0;
-        final List<String> itens = new ArrayList<>(resposta.size());
-        for (String componente: resposta) {
-            itens.add(String.format("[%d]%s", indice++, componente));
-        }
-
-        return linha(itens);
+        System.out.println(resposta.get(0));
     }
 }
